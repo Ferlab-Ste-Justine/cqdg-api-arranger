a538792c67a4db2ab983a396f045f4555561679e
Aymeric Toulouse <atoulouse@caresimple.com>
2023-03-17 15:21:44 -0400

    feat: CQDG-999 protect stats fail

--- src/services/elasticsearch.ts
+++ src/services/elasticsearch.ts
@@ -32,89 +32,124 @@ const client = new Client({
 export default client;
 
 export const fetchFileStats = async (): Promise<number> => {
-  const { body } = await client.search({
-    index: esFileIndex,
-    body: {
-      aggs: { types_count: { value_count: { field: fileIdKey } } },
-    },
-    size: 0,
-  });
+  try {
+    const { body } = await client.search({
+      index: esFileIndex,
+      body: {
+        aggs: { types_count: { value_count: { field: fileIdKey } } },
+      },
+      size: 0,
+    });
 
-  return body?.aggregations?.types_count.value;
+    return body?.aggregations?.types_count.value;
+  } catch (error) {
+    console.log('[fetchFileStats] error: ', error);
+    return null;
+  }
 };
 
 export const fetchFileSizeStats = async (): Promise<string> => {
-  const { body } = await client.search({
-    index: esFileIndex,
-    body: {
-      aggs: { types_count: { sum: { field: 'file_size' } } },
-    },
-    size: 0,
-  });
-  return filesize(body?.aggregations?.types_count.value).replace(' ', '');
+  try {
+    const { body } = await client.search({
+      index: esFileIndex,
+      body: {
+        aggs: { types_count: { sum: { field: 'file_size' } } },
+      },
+      size: 0,
+    });
+    return filesize(body?.aggregations?.types_count.value).replace(' ', '');
+  } catch (error) {
+    console.log('[fetchFileSizeStats] error: ', error);
+    return null;
+  }
 };
 
 export const fetchStudyStats = async (): Promise<number> => {
-  const { body } = await client.search({
-    index: esStudyIndex,
-    body: {
-      aggs: { types_count: { value_count: { field: studyIdKey } } },
-    },
-    size: 0,
-  });
-  return body?.aggregations?.types_count.value;
+  try {
+    const { body } = await client.search({
+      index: esStudyIndex,
+      body: {
+        aggs: { types_count: { value_count: { field: studyIdKey } } },
+      },
+      size: 0,
+    });
+    return body?.aggregations?.types_count.value;
+  } catch (error) {
+    console.log('[fetchStudyStats] error: ', error);
+    return null;
+  }
 };
 
 export const fetchParticipantStats = async (): Promise<number> => {
-  const { body } = await client.search({
-    index: esParticipantIndex,
-    body: {
-      aggs: { types_count: { value_count: { field: participantIdKey } } },
-    },
-    size: 0,
-  });
-  return body?.aggregations?.types_count.value;
+  try {
+    const { body } = await client.search({
+      index: esParticipantIndex,
+      body: {
+        aggs: { types_count: { value_count: { field: participantIdKey } } },
+      },
+      size: 0,
+    });
+    return body?.aggregations?.types_count.value;
+  } catch (error) {
+    console.log('[fetchParticipantStats] error: ', error);
+    return null;
+  }
 };
 
 export const fetchBiospecimenStats = async (): Promise<number> => {
-  const { body } = await client.search({
-    index: esBiospecimenIndex,
-    body: {
-      aggs: { types_count: { value_count: { field: biospecimenIdKey } } },
-    },
-    size: 0,
-  });
-  return body?.aggregations?.types_count.value;
+  try {
+    const { body } = await client.search({
+      index: esBiospecimenIndex,
+      body: {
+        aggs: { types_count: { value_count: { field: biospecimenIdKey } } },
+      },
+      size: 0,
+    });
+    return body?.aggregations?.types_count.value;
+  } catch (error) {
+    console.log('[fetchBiospecimenStats] error: ', error);
+    return null;
+  }
 };
 
 export const fetchVariantStats = async (): Promise<number> => {
-  const { body } = await client.search({
-    index: esVariantIndex,
-    body: {
-      aggs: { types_count: { value_count: { field: variantIdKey } } },
-    },
-    size: 0,
-  });
-  return body?.aggregations?.types_count.value;
+  try {
+    const { body } = await client.search({
+      index: esVariantIndex,
+      body: {
+        aggs: { types_count: { value_count: { field: variantIdKey } } },
+      },
+      size: 0,
+    });
+    return body?.aggregations?.types_count.value;
+  } catch (error) {
+    console.log('[fetchVariantStats] error: ', error);
+    return null;
+  }
 };
 
 export const fetchGenomicFeatureSuggestions = async (prefix: string, type: string): Promise<any> => {
-  const _index = type === SUGGESTIONS_TYPES.GENE ? indexNameGeneFeatureSuggestion : indexNameVariantFeatureSuggestion;
-  const { body } = await client.search({
-    index: _index,
-    body: {
-      suggest: {
-        suggestions: {
-          prefix,
-          completion: {
-            field: 'suggest',
-            size: maxNOfGenomicFeatureSuggestions,
+  try {
+    const _index = type === SUGGESTIONS_TYPES.GENE ? indexNameGeneFeatureSuggestion : indexNameVariantFeatureSuggestion;
+    const { body } = await client.search({
+      index: _index,
+      body: {
+        suggest: {
+          suggestions: {
+            prefix,
+            completion: {
+              field: 'suggest',
+              size: maxNOfGenomicFeatureSuggestions,
+            },
           },
         },
       },
-    },
-  });
-  return body?.suggest?.suggestions[0];
+    });
+    return body?.suggest?.suggestions[0];
+  } catch (error) {
+    console.log('[fetchGenomicFeatureSuggestions] error: ', error);
+    return null;
+  }
 };
 
 export const createIndexIfNeeded = async (esClient: Client, indexName: string): Promise<boolean> => {
