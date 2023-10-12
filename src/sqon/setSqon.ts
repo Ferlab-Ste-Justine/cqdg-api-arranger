f18dc8aa81a92941555c3f6913274051479db170
Aymeric Toulouse <atoulouse@caresimple.com>
2023-10-12 12:10:53 -0400

    feat: CQDG-394 refacto replaceSetByIds handleContentRecursively

--- src/sqon/setSqon.ts
+++ src/sqon/setSqon.ts
@@ -14,32 +14,52 @@ const getPathToParticipantId = (type: string) => {
   }
 };
 
+interface ISqonContent {
+  content: {
+    field: string;
+    index: string;
+    value: any[];
+    remoteComponent: any[];
+  };
+  op: 'in';
+}
+
+const handleContent = async (content: ISqonContent, accessToken: string, userId: string) => {
+  try {
+    const contents = [];
+    const matches = setRegex.exec(content?.content.value ? content.content.value[0] : '');
+    const setId = matches && matches[1] ? matches[1] : null;
+    if (setId) {
+      const set = await getUserSet(accessToken, userId, setId);
+      const newContent = { ...content };
+      newContent.content.field = getPathToParticipantId(set.content.setType);
+      newContent.content.value = set.content.ids;
+      contents.push(newContent);
+    } else {
+      contents.push(content);
+    }
+    return contents;
+  } catch (error) {
+    console.error('[handleContent] content:', content, error);
+  }
+};
+
+const handleContentRecursively = async (content: any, accessToken: string, userId: string) => {
+  const contents = [];
+  if (Array.isArray(content)) {
+    for (const deepContent of content) {
+      contents.push(...(await handleContentRecursively(deepContent, accessToken, userId)));
+    }
+  } else {
+    contents.push(...(await handleContent(content, accessToken, userId)));
+  }
+  return contents;
+};
+
 export const replaceSetByIds = async (sqon: SetSqon, accessToken: string, userId: string) => {
   if (!sqon) {
     throw new Error('Sqon is missing');
   }
-  const contents = [];
-
-  for (const content of sqon.content) {
-    const handleContent = async content => {
-      if (content?.content?.value[0].match(setRegex)) {
-        const match = setRegex.exec(content.content.value[0])[1];
-        const set = await getUserSet(accessToken, userId, match);
-        const newContent = { ...content };
-        newContent.content.field = getPathToParticipantId(set.content.setType);
-        newContent.content.value = set.content.ids;
-        contents.push(newContent);
-      } else {
-        contents.push(content);
-      }
-    };
-    if (Array.isArray(content.content)) {
-      for (const deepContent of content.content) {
-        await handleContent(deepContent);
-      }
-    } else {
-      await handleContent(content);
-    }
-  }
+  const contents = await handleContentRecursively(sqon.content, accessToken, userId);
   return { op: 'and', content: contents };
 };
